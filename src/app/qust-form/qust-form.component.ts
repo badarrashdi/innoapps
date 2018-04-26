37f831655f9e8954d554e315fd46f39482dc9a43
badar.monir <badar.monir@osscube.com>
2018-04-26 16:25:16 +0530

    form

--- src/app/qust-form/qust-form.component.ts
+++ src/app/qust-form/qust-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormDataService } from './data/formData.service';
+
 @Component({
   selector: 'app-qust-form',
   templateUrl: './qust-form.component.html',
@@ -7,15 +7,40 @@ import { FormDataService } from './data/formData.service';
 })
 export class QustFormComponent implements OnInit {
 
-  title = 'Multi-Step Wizard';
-  @Input() formData;
-  
-  constructor(private formDataService: FormDataService) {
+  tabs;
+  currentTab = 1;
+  steps = ['', '', '', ''];
+
+
+
+  constructor() {
+  }
+  tabswitch(a) {
+    this.currentTab = a;
+    this.steps[a - 2] = "Done";
+    this.steps[a-1] = "";
+    if (a == 2) {
+      this.steps[0] = "Done";
+    }
+
+  }
+
+  editStep(stepToEdit) {
+    let temp = stepToEdit;
+
+    for (let i = 0; i < this.steps.length; i++) {
+      if (temp - 1 == i) {
+        this.steps[i] = "";
+        this.currentTab = stepToEdit;
+      } else {
+        this.steps[i] = "Done";
+      }
+
+    }
   }
 
   ngOnInit() {
-      this.formData = this.formDataService.getFormData();
-      console.log(this.title + ' loaded!');
+
   }
 
 }
